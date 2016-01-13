4ad915318ff0401e504917a4973dcbca480f683d
rxl194 <Rendong_Lin@yahoo.com>
2016-01-13 04:26:47 -0800

    MongoLab

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bj101
+
+# mongodb setup
+    // git clone git://github.com/mongolab/hello-mongoose.git && cd hello-mongoose
+    //heroku create
+    heroku addons:add mongolab
+    git push heroku master
+    heroku open
+
+# end -- mongodb setup
+
--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,7 @@ var config = require('./config'),
 module.exports = function() {
   var db = mongoose.connect(config.db, function(err,res) {
     if (err) { 
-      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      console.log ('ERROR connecting to: ' + config.db + '. ' + err);
     } else {
       console.log ('mongodb connected');
     }
