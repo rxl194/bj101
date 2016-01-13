ce982eb3e4dcb0b4600f6ec4e9e05bf8a23b1090
rxl194 <Rendong_Lin@yahoo.com>
2016-01-13 18:04:26 -0500

    passport-local

--- app/routes/ys_users.server.routes.js
+++ app/routes/ys_users.server.routes.js
@@ -1,17 +1,70 @@
-var users = require('../../app/controllers/ys_users.server.controller');
+// Invoke 'strict' JavaScript mode
+'use strict';
 
+// Load the module dependencies
+var users = require('../../app/controllers/ys_users.server.controller'),
+	passport = require('passport');
+
+// Define the routes module' method
 module.exports = function(app) {
+
   app.route('/ys/users')
     .post(users.create)
     .get(users.list);
 
-  app.route('/ys/users/:userId')
+   app.route('/ys/users/:userId')
      .get(users.read)
      .put(users.update)
      .delete(users.delete);
 
   app.param('userId', users.userByID);
 
+	// Set up the 'signup' routes 
+	app.route('/ys/signup')
+	   .get(users.renderSignup)
+	   .post(users.signup);
 
+	// Set up the 'signin' routes 
+	app.route('/ys/signin')
+	   .get(users.renderSignin)
+	   .post(passport.authenticate('local', {
+			successRedirect: '/',
+			failureRedirect: '/ys/signin',
+			failureFlash: true
+	   }));
+
+	// Set up the Facebook OAuth routes 
+	app.get('/oauth/facebook', passport.authenticate('facebook', {
+		failureRedirect: '/ys/signin'
+	}));
+	app.get('/oauth/facebook/callback', passport.authenticate('facebook', {
+		failureRedirect: '/ys/signin',
+		successRedirect: '/'
+	}));
+
+	// Set up the Twitter OAuth routes 
+	app.get('/oauth/twitter', passport.authenticate('twitter', {
+		failureRedirect: '/ys/signin'
+	}));
+	app.get('/oauth/twitter/callback', passport.authenticate('twitter', {
+		failureRedirect: '/ys/signin',
+		successRedirect: '/'
+	}));
+
+	// Set up the Google OAuth routes 
+	app.get('/oauth/google', passport.authenticate('google', {
+		scope: [
+			'https://www.googleapis.com/auth/userinfo.profile',
+			'https://www.googleapis.com/auth/userinfo.email'
+		],
+		failureRedirect: '/ys/signin'
+	}));
+	app.get('/oauth/google/callback', passport.authenticate('google', {
+		failureRedirect: '/ys/signin',
+		successRedirect: '/'
+	}));
+
+	// Set up the 'signout' route
+	app.get('/ys/signout', users.signout);
 };
 
--- server.js
+++ server.js
@@ -4,10 +4,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var PORT = process.env.PORT || 3000;
 
 var mongoose = require('./config/mongoose'),
-    express = require('./config/express');
+    express = require('./config/express'),
+    passport = require('./config/passport');
 
 var db = mongoose();
 var app = express();
+var passport = passport();
+
 app.listen(PORT);
 module.exports = app;
 
