961f25e3567e6e137477a1396280ce67369938ce
rxl194 <Rendong_Lin@yahoo.com>
2016-01-18 18:53:38 -0800

    checked

--- config/express.js
+++ config/express.js
@@ -3,62 +3,80 @@
 
 // Load the module dependencies
 var config = require('./config'),
-	express = require('express'),
-	morgan = require('morgan'),
-	compress = require('compression'),
-	bodyParser = require('body-parser'),
-	methodOverride = require('method-override'),
-	session = require('express-session'),
+  http = require('http'),
+  socketio = require('socket.io'),
+  express = require('express'),
+  morgan = require('morgan'),
+  compress = require('compression'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  session = require('express-session'),
+  MongoStore = require('connect-mongo')(session),
   flash = require('connect-flash'),
-	passport = require('passport');
+  passport = require('passport');
 
 // Define the Express configuration method
-module.exports = function() {
-	// Create a new Express application instance
-	var app = express();
+module.exports = function(db) {
+  // Create a new Express application instance
+  var app = express();
 
-	// Use the 'NDOE_ENV' variable to activate the 'morgan' logger or 'compress' middleware
-	if (process.env.NODE_ENV === 'development') {
-		app.use(morgan('dev'));
-	} else if (process.env.NODE_ENV === 'production') {
-		app.use(compress());
-	}
+  // Create a new HTTP server
+  var server = http.createServer(app);
 
-	// Use the 'body-parser' and 'method-override' middleware functions
-	app.use(bodyParser.urlencoded({
-		extended: true
-	}));
-	app.use(bodyParser.json());
-	app.use(methodOverride());
+  // Create a new Socket.io server
+  var io = socketio.listen(server);
 
-	// Configure the 'session' middleware
-	app.use(session({
-		saveUninitialized: true,
-		resave: true,
-		secret: config.sessionSecret
-	}));
+  // Use the 'NDOE_ENV' variable to activate the 'morgan' logger or 'compress' middleware
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
 
-	// Set the application view engine and 'views' folder
-	app.set('views', './app/views');
-	app.set('view engine', 'ejs');
+  // Use the 'body-parser' and 'method-override' middleware functions
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
 
-	// Configure the flash messages middleware
-	app.use(flash());
+  // Configure the MongoDB session storage
+  var mongoStore = new MongoStore({
+    db: db.connection.db
+  });
 
-	// Configure the Passport middleware
-	app.use(passport.initialize());
-	app.use(passport.session());
+  // Configure the 'session' middleware
+  app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: config.sessionSecret,
+    store: mongoStore
+  }));
 
-	// Load the routing files
-	require('../app/routes/index.routes.js')(app);
-	require('../app/routes/ys/ys_users.routes.js')(app);
-	require('../app/routes/bo/bo_articles.routes.js')(app);
+  // Set the application view engine and 'views' folder
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
 
-	// Configure static file serving
-	app.use(express.static('./public'));
+  // Configure the flash messages middleware
+  app.use(flash());
 
-	// Return the Express application instance
-	return app;
+  // Configure the Passport middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Load the routing files
+  require('../app/routes/index.routes.js')(app);
+  require('../app/routes/ys/ys_users.routes.js')(app);
+  require('../app/routes/bo/bo_articles.routes.js')(app);
+
+  // Configure static file serving
+  app.use(express.static('./public'));
+
+  // Load the Socket.io configuration
+  require('./socketio')(server, io, mongoStore);
+
+  // Return the Server instance
+  return server;
 };
 
 
