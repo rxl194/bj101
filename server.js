326a114da8cb48631230b06d60f8b3b650f6c10b
rxl194 <Rendong_Lin@yahoo.com>
2016-01-18 18:50:32 -0800

    server.js v0

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var PORT = process.env.PORT || 3000;
 
+// Load the module dependencies
 var mongoose = require('./config/mongoose'),
     express = require('./config/express'),
     passport = require('./config/passport');
 
+// Create a new Mongoose connection instance
 var db = mongoose();
-var app = express();
+
+// Create a new Express application instance
+var app = express(db);
+
+// Configure the Passport middleware
 var passport = passport();
 
 app.listen(PORT);
