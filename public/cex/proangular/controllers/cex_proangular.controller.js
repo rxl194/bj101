f482e3f6eb183f76578148b62d0e1c91228bc792
rxl194 <Rendong_Lin@yahoo.com>
2016-01-26 01:02:53 +0000

    cex-prograngula-controller moved to coa-todolists-controller

--- public/cex/proangular/controllers/cex_proangular.controller.js
+++ public/cex/proangular/controllers/cex_proangular.controller.js
@@ -2,30 +2,8 @@
 'use strict';
 
 // Create the 'cex_proangular' controller
-angular.module('cex_proangular').controller('cexProangularController', ['$scope', 'Authentication',
-  function($scope, Authentication) {
-    // Expose the authentication service
-    $scope.authentication = Authentication;
-    $scope.todo = window.model;
-
-    $scope.incompleteCount = function() {
-      var count = 0;
-      angular.forEach($scope.todo.items, function(item) {
-        if (!item.done) {
-          count++
-        }
-      });
-      return count;
-    }
-
-    $scope.warningLevel = function () {
-      return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
-    }            
-
-    $scope.addNewItem = function (actionText) {
-      $scope.todo.items.push({ action: actionText, done: false });
-    }
-
+angular.module('cex_proangular').controller('cexProangularController', ['$scope', 
+  function($scope) {
 
   }
 ]);
--- public/coa/todolists/controllers/coa_todolists.controller.js
+++ public/coa/todolists/controllers/coa_todolists.controller.js
@@ -3,15 +3,35 @@
 
 // Create the 'coa_todolists' controller
 angular.module('coa_todolists').controller('coaTodolistsController', ['$scope', 'Authentication', 
-    function($scope, Authentication) {
-    	// Expose the Authentication service
-        $scope.authentication = Authentication;
+  function($scope, Authentication) {
+    // Expose the authentication service
+    $scope.authentication = Authentication;
+    $scope.todo = window.model;
 
-        // Create a new controller method for retrieving a list of articles
-        $scope.find = function() {
-        	// Use the article 'query' method to send an appropriate GET request
-            $scope.articles = Articles.query();
-        };
+    $scope.incompleteCount = function() {
+      var count = 0;
+      angular.forEach($scope.todo.items, function(item) {
+        if (!item.done) {
+          count++
+        }
+      });
+      return count;
     }
+
+    $scope.warningLevel = function () {
+      return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+    }            
+
+    $scope.addNewItem = function (actionText) {
+      $scope.todo.items.push({ action: actionText, done: false });
+    }
+
+    // Create a new controller method for retrieving a list of articles
+    $scope.find = function() {
+      // Use the article 'query' method to send an appropriate GET request
+      $scope.articles = Articles.query();
+    };
+
+  }
 ]);
 
