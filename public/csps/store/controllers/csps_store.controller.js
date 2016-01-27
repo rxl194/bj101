90db5d32d08a7b90b04970a090f8daa14643c434
rxl194 <Rendong_Lin@yahoo.com>
2016-01-27 02:34:47 +0000

    .

---
+++ public/csps/store/controllers/csps_store.controller.js
@@ -0,0 +1,28 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Create the 'csps_store' controller
+angular.module('csps_store').controller('cspsControllerController', 
+           ['$scope', '$location', 'Authentication', 
+  function($scope, $location, Authentication) {
+    // Expose the Authentication service
+    $scope.authentication = Authentication;
+
+    // Create a new controller method for retrieving a list of products
+    $scope.find = function() {
+      // Use the csps_products 'query' method to send an appropriate GET request
+      $scope.data = { };
+      $scope.data.products = [
+            { name: "Product #1", description: "A product",
+                category: "Category #1", price: 100 },
+            { name: "Product #2", description: "A product",
+                category: "Category #1", price: 110 },
+            { name: "Product #3", description: "A product",
+                category: "Category #2", price: 210 },
+            { name: "Product #4", description: "A product",
+                category: "Category #3", price: 202 }
+      ];
+    };
+  }
+]);
+
