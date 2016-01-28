cb9e7cf37ac239ea3aeed818a9d571af05e97d34
rxl194 <Rendong_Lin@yahoo.com>
2016-01-28 19:13:50 +0000

    .

--- public/csps/store/config/csps_store.routes.js
+++ public/csps/store/config/csps_store.routes.js
@@ -8,8 +8,14 @@ angular.module('csps_store').config(['$routeProvider',
 		when('/csps/checkout', {
 			templateUrl: '/csps/cart/views/csps_checkout.view.html'
 		}).
+		when('/csps/placeorder', {
+			templateUrl: '/csps/cart/views/csps_placeorder.view.html'
+		}).
 		when('/csps/products', {
 			templateUrl: '/csps/products/views/csps_catproduct.view.html'
+		}).
+		when('/csps/complete', {
+			templateUrl: '/csps/products/views/csps_thankyou.view.html'
 		}).
 		otherwise({
 			templateUrl: '/csps/products/views/csps_catproduct.view.html'
