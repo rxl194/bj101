d93d5bbc0eea4a791a51646205dec99db55c466d
rxl194 <Rendong_Lin@yahoo.com>
2016-01-28 00:15:20 +0000

    ng-include

--- public/csps/store/controllers/csps_store.controller.js
+++ public/csps/store/controllers/csps_store.controller.js
@@ -7,6 +7,8 @@ angular.module('csps_store').controller('cspsStoreController',
   function($scope, $location, Authentication, cspsProducts) {
     // Expose the Authentication service
     $scope.authentication = Authentication;
+    $scope.pathCspsCatProdView = '/csps/products/views/csps_catproduct.view.html';
+    $scope.pathExProangularSps = '/ex/proangular/sportsstore';
 
     // Create a new controller method for retrieving a list of products
     $scope.find = function() {
