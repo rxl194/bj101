d45126d32976eef7aa0a33164dfb155e621b3e12
rxl194 <Rendong_Lin@yahoo.com>
2016-01-29 23:36:41 +0000

    .

--- app/controllers/ex/proangular/ex_proangular.sps_admin.controller.js
+++ app/controllers/ex/proangular/ex_proangular.sps_admin.controller.js
@@ -5,15 +5,18 @@
 exports.render = function(req, res) {
   var myTitle = 'Sports Store Admin';
   var fullName = 'Adam Developer';
-  if ( req.user ) {
+  var adminUser = null;
+  if (req.user) {
     fullName = req.user.fullName;
     myTitle = fullName + "\'s " + myTitle;
+    if (req.user.provider === 'local' && req.user.username === 'fl2')
+      adminUser = JSON.stringify(req.user);
   }
 
   // Use the 'response' object to render the 'index' view with a 'title' and 'userFullName' properties
   res.render('ex/proangular/sps_admin', {
     title: myTitle,
-    cys_user: JSON.stringify(req.user),
+    cys_user: adminUser
   });
 };
 
