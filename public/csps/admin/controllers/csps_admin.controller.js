3c6621d5bf1d31442226bdc9fcd4ec50dd0ff186
rxl194 <Rendong_Lin@yahoo.com>
2016-01-29 23:41:07 +0000

    .

---
+++ public/csps/admin/controllers/csps_admin.controller.js
@@ -0,0 +1,13 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Create the 'csps_admin' controller
+angular.module('csps_admin')
+.controller('cspsAdminController', 
+         ['$scope', 'Authentication', 
+
+  function($scope, Authentication) {
+  }
+
+]);
+
