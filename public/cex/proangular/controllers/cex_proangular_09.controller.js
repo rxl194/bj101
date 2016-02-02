e276982ba45fe8c7678278ab20a61250a8c451b8
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-02-01 22:54:03 -0800

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

---
+++ public/cex/proangular/controllers/cex_proangular_09.controller.js
@@ -0,0 +1,7 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+angular.module("cex_proangular_09")
+  .controller("cexProangula09DayCtrl", function($scope) {
+    // controller statements will go here
+  });
