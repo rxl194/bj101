bd11215d2535a57087757c5f7f3e908f29d2101a
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-02-02 12:36:51 -0800

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

--- public/cex/proangular/ch10/controllers/cexPa10.controller.js
+++ public/cex/proangular/ch10/controllers/cexPa10.controller.js
@@ -3,6 +3,8 @@
 
 angular.module("cexPa010App")
 .controller("cexPa10DefaultCtrl", function ($scope) {
+  $scope.data = {};
+  
   $scope.todos = [
     { action: "Get groceries", complete: false },
     { action: "Call plumber", complete: false },
@@ -13,4 +15,8 @@ angular.module("cexPa010App")
   $scope.viewFile = function() {
     return $scope.showList ? "L13_list.html" : "L11_table.html";    
   };
+
+  $scope.reportChange = function () {
+    console.log("Displayed content: " + $scope.viewFile());
+  }  
 });
