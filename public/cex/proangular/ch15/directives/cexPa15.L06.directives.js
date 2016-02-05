5d5b7d1fb379691f78577c65eb30f74edfcc113e
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-02-05 13:56:08 -0800

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

---
+++ public/cex/proangular/ch15/directives/cexPa15.L06.directives.js
@@ -0,0 +1,21 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+var app = angular.module("cexPa15App");
+
+app.directive("cexPa15UnorderedList", function () {
+  return function (scope, element, attrs) {
+    var data = scope[attrs["cexPa15UnorderedList"]];
+    var propertyName = attrs["listProperty"];    
+    if (angular.isArray(data)) {
+      var listElem = angular.element("<ul>");
+      element.append(listElem);      
+      for (var i = 0; i < data.length; i++) {
+        // console.log("Item: " + data[i].name);
+        // listElem.append(angular.element('<li>').text(data[i].name));          
+        listElem.append(angular.element('<li>').text(data[i][propertyName]));
+      }
+    }
+  }
+});
+
