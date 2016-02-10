5fd73da17f8a638e72faba812d9b72d67bf33bba
rxl194 <Rendong_Lin@yahoo.com>
2016-02-09 17:14:11 -0800

    Signed-off-by: rxl194 <Rendong_Lin@yahoo.com>

--- app/routes/ex/m101js/ex_m101js.w01.controller.js
+++ app/routes/ex/m101js/ex_m101js.w01.controller.js
@@ -5,8 +5,48 @@
 var mongoose = require('mongoose'),
 	EnMovie = mongoose.model('enMovie');
 
+// Create a new error handling controller method
+var getErrorMessage = function(err) {
+  if (err.errors) {
+    for (var errName in err.errors) {
+      if (err.errors[errName].message) return err.errors[errName].message;
+    }
+  } else {
+    return 'Unknown server error';
+  }
+};
+
+// Create a new controller method that retrieves a list of enMovies
+exports.list = function(req, res) {
+
+  // Use the model 'find' method to get a list of enMovies
+  EnMovie.find().exec(function(err, enMovies) {
+    if (err) {
+      // If an error occurs send the error message
+      return res.status(400).send({
+        message: getErrorMessage(err)
+      });
+    } else {
+      // Send a JSON representation of the enMovie 
+      res.json(enMovies);
+    }
+  });
+
+};
+
+
 // Create a new 'render' controller method
 exports.render = function(req, res) {
+
+  // Use the model 'find' method to get a list of enMovies
+  EnMovie.find().exec(function(err, movies) {
+
+	  // Use the 'response' object to render the 'index' view with a 'title' and 'userFullName' properties
+	  res.render('ex/m101js/w01/movies', {
+		  title: 'ex_m101js w01 Movies',
+		  enMovies: movies
+	  });
+  });
 
 };
 
--- app/routes/ex/m101js/ex_m101js.w01.routes.js
+++ app/routes/ex/m101js/ex_m101js.w01.routes.js
@@ -3,8 +3,13 @@
 
 // Define the routes module' method
 module.exports = function(app) {
-  var ex_m101js = require('./ex_m101js.w01.controller');
-  app.get('/ex/m101js/w01', ex_m101js.render);
 
+  var ex_m101js = require('./ex_m101js.w01.controller');
+
+  // Set up the 'movies' base routes 
+	app.route('/api/ex/m101js/enmovies')
+	   .get(ex_m101js.list);
+
+  app.get('/ex/m101js/w01', ex_m101js.render);
 };
 
