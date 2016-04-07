368a66bfd42c5ffcb5690d74f01707a8148557e5
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-04-07 14:45:20 -0700

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

--- public/cex/proangular/ch25/L07/app.js
+++ public/cex/proangular/ch25/L07/app.js
@@ -21,4 +21,16 @@ angular.module("exampleApp", [])
   $scope.incrementCounter = function() {
       $scope.counter++;
   }
+})
+.filter("labelCase", function () {
+  return function (value, reverse) {
+    if (angular.isString(value)) {
+      var intermediate = reverse ? value.toUpperCase() : value.toLowerCase();
+      return (reverse ? intermediate[0].toLowerCase() :
+          intermediate[0].toUpperCase()) + intermediate.substr(1);
+    } else {
+      return value;
+    }
+  };
 });
+
