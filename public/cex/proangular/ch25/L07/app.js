f830fd3436146ae6683f2772f633371d72c2916e
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-04-07 15:03:57 -0700

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

--- public/cex/proangular/ch25/L07/app.js
+++ public/cex/proangular/ch25/L07/app.js
@@ -44,5 +44,18 @@ angular.module("exampleApp", [])
       }
     }
   }
+})
+.factory("counterService", function () {
+  var counter = 0;
+  return {
+    incrementCounter: function () {
+        counter++;
+    },
+    getCounter: function() {
+        return counter;
+    }
+  }
 });
+
+
 
