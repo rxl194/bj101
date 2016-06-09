746d5e790d4014e2b323ae3168c321197eba171e
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-06-09 15:54:10 -0700

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

--- public/cex/angularjs01/P2_C04/js/angularjs01P2_app.js
+++ public/cex/angularjs01/P2_C04/js/angularjs01P2_app.js
@@ -5,7 +5,7 @@ angular.module('angularjs01P2_7minWorkout', []);
 
 angular.module('angularjs01P2_appapp', ['ngRoute', 'angularjs01P2_7minWorkout'])
 .config(function ($routeProvider) {
-    var partial_pre = 'cex/angularjs01/P2_C04/partials';
+    var partial_pre = '/cex/angularjs01/P2_C04/partials';
     $routeProvider.when('/start', { templateUrl: partial_pre+'/start.html' });
     $routeProvider.when('/workout', { templateUrl: partial_pre+'/workout.html', controller: 'AngularJs01P2WorkoutController' });
     $routeProvider.when('/finish', { templateUrl: partial_pre+'/finish.html' });
