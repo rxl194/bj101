2ee85bf945d9afb1c768845a19ee966b659b40f4
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-06-13 17:48:25 -0700

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

---
+++ public/cex/angularjs01/P2_C07/js/angularjs01P2_app.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+angular.module('angularjs01P2_7minWorkout', []);
+
+angular.module('angularjs01P2_appapp', ['ngRoute', 'angularjs01P2_7minWorkout'])
+.config(function ($routeProvider, $sceDelegateProvider) {
+  
+  var partial_pre = '/cex/angularjs01/P2_C07/partials';
+  $routeProvider.when('/start', { templateUrl: partial_pre+'/start.html' });
+  $routeProvider.when('/workout', { templateUrl: partial_pre+'/workout.html', controller: 'AngularJs01P2WorkoutController' });
+  $routeProvider.when('/finish', { templateUrl: partial_pre+'/finish.html' });
+  $routeProvider.otherwise({ redirectTo: '/start' });
+    
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    'http://*.youtube.com/**']);
+        
+});
+
+
+
